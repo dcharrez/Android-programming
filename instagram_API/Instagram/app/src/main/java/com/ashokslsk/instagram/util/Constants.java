9df2b8fa309ee41892456827304ec44ca44dc11d
Diego <dcharrezt@unsa.edu.pe>
2018-10-24 09:05:08 -0500

    instagram

---
+++ instagram_API/Instagram/app/src/main/java/com/ashokslsk/instagram/util/Constants.java
@@ -0,0 +1,13 @@
+package com.ashokslsk.instagram.util;
+
+/**
+ * Created by ashok.kumar on 04/02/16.
+ */
+public class Constants {
+
+    public static final String CLIENT_ID = "YOUR_CLIENT_KEY";
+    public static final String CLIENT_SECRET = "YOUR_CLEINT_SECRET";
+    public static final String CALLBACK_URL = "instagram://connect";
+
+
+}
